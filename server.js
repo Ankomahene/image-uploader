a085bcc0038d73d7dd7a8dba8bce8859d4cf6d78
Ankomahene <ankomaheneshadrack@gmail.com>
2020-04-29 23:56:32 +0000

    ready to deploy

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
+const path = require('path');
 
 const app = express();
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'image-upload/build')));
 
 app.route('/api/upload').get(controller.sendMessage).post(controller.uploadImage);
 
-const port = 5000;
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/image-upload/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server has started on port: ${port}`));
